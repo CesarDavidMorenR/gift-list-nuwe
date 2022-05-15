daaecea0781d439eb7d3b65397a41cc55b465070
cesar david moreno <cesardaviddev@gmail.com>
2022-05-15 23:10:35 +0200

    Making horizontal scrolling for pc

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,5 @@
 import React from 'react';
 // import "./App.css";
-import { Card } from "./Card.js";
 import { Tag } from "./Tag.js";
 import { Description } from "./Description.js";
 import { Header } from "./Header.js";
@@ -10,11 +9,13 @@ import { Header } from "./Header.js";
 
 export const Container = () => {
   return (
-    <div className="cardsContainer">
-      <Header />
-      <Description />
-      <Tag />
-    </div>
+    <section className="overflowx">
+      <div className="cardsContainer">
+        <Header />
+        <Description />
+        <Tag />
+      </div>
+    </section>
   );
 }
 
